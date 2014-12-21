1a1d82d7cd93cd426233dc4a73cf477b14d02f8e
Steven Crockett <crockett.j.steven@gmail.com>
2014-12-21 14:10:40 +0000

    Added 3 basic sorting algorithms (selection sort, bubble sort, insertion sort) with accompanying tests.

---
+++ src/main/java/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,52 @@
+package algorithms.sorting;
+
+import java.util.Comparator;
+
+/**
+ * Generic implementation of bubble sort, sorting items in ascending order.
+ */
+public class BubbleSort {
+
+    /**
+     * Sort the given items in ascending order.
+     * Items implementing comparable can be easily sorted.
+     *
+     * @param items The items to be sorted.
+     */
+    public static void sort(Comparable[] items) {
+        sort(items, Comparable::compareTo);
+    }
+
+    /**
+     * Sort the given items in ascending order.
+     * For items not implementing comparable, a function needs to be given in
+     * order to compare the items and provide an ordering.
+     *
+     * @param items The items to be sorted.
+     * @param compare Function to compare the given items.
+     * @param <T> Type of the items to be sorted.
+     */
+    public static <T> void sort(T[] items, Comparator<T> compare) {
+
+        final int itemCount = items.length;
+
+        // need to sort all but one item. once everything is sorted, the final
+        // item will clearly be in the correct position
+        for (int i = 0, iterations = itemCount - 1; i < iterations; i++) {
+
+            // after the ith iteration, the last i items in the array must be in sorted
+            // order, so we can avoid checking these positions
+            for (int j = 0, end = iterations - i; j < end; j++) {
+
+                // compare adjacent items in the array and swap if out of order
+                if (compare.compare(items[j], items[j + 1]) > 0) {
+                    final T temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ src/main/java/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,55 @@
+package algorithms.sorting;
+
+import java.util.Comparator;
+
+/**
+ * Generic implementation of selection sort, sorting items in ascending order.
+ */
+public class SelectionSort {
+
+    /**
+     * Sort the given items in ascending order.
+     * Items implementing comparable can be easily sorted.
+     *
+     * @param items The items to be sorted.
+     */
+    public static void sort(Comparable[] items) {
+        sort(items, Comparable::compareTo);
+    }
+
+    /**
+     * Sort the given items in ascending order.
+     * For items not implementing comparable, a function needs to be given in
+     * order to compare the items and provide an ordering.
+     *
+     * @param items The items to be sorted.
+     * @param compare Function to compare the given items.
+     * @param <T> Type of the items to be sorted.
+     */
+    public static <T> void sort(T[] items, Comparator<T> compare) {
+
+        final int itemCount = items.length;
+
+        // for each index, find the smallest remaining element and place it at the index
+        for (int i = 0, iterations = itemCount - 1; i < iterations; i++) {
+
+            int currentMinIndex = i;
+            T currentMin = items[i];
+
+            // iterate forward to the end of the array, finding the min value
+            for (int j = i + 1; j < itemCount; j++) {
+                if (compare.compare(currentMin, items[j]) > 0) {
+                    currentMinIndex = j;
+                    currentMin = items[j];
+                }
+            }
+
+            // finally, place the min value at the smallest unsorted index.
+            T temp = items[i];
+            items[i] = items[currentMinIndex];
+            items[currentMinIndex] = temp;
+
+        }
+
+    }
+}
