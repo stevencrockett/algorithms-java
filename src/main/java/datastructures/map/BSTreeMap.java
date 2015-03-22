c518d8add96e7d33dbf38e8f161149368a77841d
Steven Crockett <crockett.j.steven@gmail.com>
2015-03-22 04:31:33 +0000

    Added more documentation and some code tweaks/clean-ups.

--- src/main/java/datastructures/map/BSTreeMap.java
+++ src/main/java/datastructures/map/BSTreeMap.java
@@ -6,13 +6,35 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * Ordered map implemented using an unbalanced binary search tree. Since balance isn't preserved, depth
  * of the tree may vary by the order in which keys are inserted.
+ *
+ * @param <Key> Type of the keys stored in the map which can be compared and ordered.
+ * @param <Value> Type of the values stored in the map.
  */
 public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap<Key, Value> {
 
     /**
+     * A node in the binary tree, storing a key-value pair and pointers to its parent and children.
+     *
+     * @param <K> Type of the key stored in the node which can be compared and ordered.
+     * @param <V> Type of the value stored in the node.
+     */
+    private static class Node<K extends Comparable<K>, V> {
+        private K key;
+        private V value;
+        private Node<K, V> parent, left, right;
+
+        public Node(final K key, final V value, final Node<K, V> parent) {
+            this.key = key;
+            this.value = value;
+            this.parent = parent;
+        }
+    }
+
+
+    /**
      * Root node of the BST.
      */
-    private Node root;
+    private Node<Key, Value> root;
 
 
     /**
@@ -30,9 +52,9 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
     @Override
     public void put(final Key key, final Value value) {
 
-        Node parent = null;
+        Node<Key, Value> parent = null;
 
-        Node x = root;
+        Node<Key, Value> x = root;
 
         while (x != null) {
             final int cmp = key.compareTo(x.key);
@@ -51,11 +73,11 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
         }
 
         if (parent == null) {
-            root = new Node(key, value, null);
+            root = new Node<>(key, value, null);
         } else if (key.compareTo(parent.key) < 0) {
-            parent.left = new Node(key, value, parent);
+            parent.left = new Node<>(key, value, parent);
         } else {
-            parent.right = new Node(key, value, parent);
+            parent.right = new Node<>(key, value, parent);
         }
 
         size++;
@@ -68,7 +90,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
     @Override
     public Optional<Value> get(final Key key) {
 
-        final Node keyNode = search(key);
+        final Node<Key, Value> keyNode = search(key);
 
         if (keyNode != null) {
             return Optional.of(keyNode.value);
@@ -83,7 +105,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      */
     @Override
     public void delete(final Key key) {
-        final Node keyNode = search(key);
+        final Node<Key, Value> keyNode = search(key);
 
         // there's nothing to delete
         if (keyNode == null) {
@@ -99,61 +121,63 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      *
      * @param node Node in the tree to be deleted.
      */
-    private void deleteNode(final Node node) {
+    private void deleteNode(final Node<Key, Value> node) {
         // 3 cases to consider
 
-        // 1. node doesn't have children. can trivially be deleted by removing pointer from parent
+        // case 1: node doesn't have children. can trivially be deleted by removing pointer from parent
         if (node.left == null && node.right == null) {
-            if (node.parent == null) {
+            if (node == root) {
                 root = null;
             } else if (node == node.parent.left){
                 node.parent.left = null;
             } else {
                 node.parent.right = null;
             }
-            return;
-        }
-
-        // 2. node has one child. make node's parent point to its single child
-        if (node.left != null && node.right == null) {
-            if (node.parent == null) {
+        } else if (node.left != null && node.right == null) {
+            // case 2: node has one child. make node's parent point to its single child
+            // has single left child
+            if (node == root) {
                 root = node.left;
+                root.parent = null;
             } else if (node == node.parent.left){
                 node.parent.left = node.left;
+                node.left.parent = node.parent; // make node point to its grandparent
             } else {
                 node.parent.right = node.left;
+                node.left.parent = node.parent; // make node point to its grandparent
             }
-            return;
         } else if (node.left == null) {
-            // node.right is always non-null at this point in control flow
-            if (node.parent == null) {
+            // case 2 continued: node has single right child
+            if (node == root) {
                 root = node.right;
+                root.parent = null;
             } else if (node == node.parent.right){
                 node.parent.right = node.right;
+                node.right.parent = node.parent; // make node point to its grandparent
             } else {
                 node.parent.left = node.right;
+                node.right.parent = node.parent; // make node point to its grandparent
             }
-            return;
-        }
-
-        // 3. node has both children.
-
-        // predecessor or successor should be moved to where the deleted node is to preserve binary search property.
-        // copy it to the deleted node location, then delete the node at its previous location.
-        // choose randomly between predecessor and successor to help preserve tree balance.
-
-        Node swappedNode;
-        if (ThreadLocalRandom.current().nextBoolean()) {
-            swappedNode = predecessorNode(node);
         } else {
-            swappedNode = successorNode(node);
-        }
-        node.key = swappedNode.key;
-        node.value = swappedNode.value;
+            // case 3: node has both children.
 
-        // recursively, call delete. will simplify to one of the first two cases as
-        // the predecessor or successor can't have two children
-        deleteNode(swappedNode);
+            // predecessor or successor should be moved to where the deleted node is to preserve binary search property.
+            // copy it to the deleted node location, then delete the node at its previous location.
+            // choose randomly between predecessor and successor to help preserve tree balance.
+
+            Node<Key, Value> swappedNode;
+            if (ThreadLocalRandom.current().nextBoolean()) {
+                swappedNode = predecessorNode(node);
+            } else {
+                swappedNode = successorNode(node);
+            }
+            node.key = swappedNode.key;
+            node.value = swappedNode.value;
+
+            // recursively, call delete. will simplify to one of the first two cases as
+            // the predecessor or successor can't have two children
+            deleteNode(swappedNode);
+        }
     }
 
 
@@ -181,9 +205,9 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      * @param key Key to search for in the tree.
      * @return Node containing the key.
      */
-    private Node search(final Key key) {
+    private Node<Key, Value> search(final Key key) {
 
-        Node x = root;
+        Node<Key, Value> x = root;
 
         while (x != null) {
             final int cmp = key.compareTo(x.key);
@@ -210,7 +234,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
             return Optional.empty();
         }
 
-        final Node min = minNode(root);
+        final Node<Key, Value> min = minNode(root);
         return Optional.of(min.key);
     }
 
@@ -220,8 +244,8 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      * @param root Node that represents the root of the tree to search.
      * @return Leftmost (i.e. minimum) node in the tree.
      */
-    private Node minNode(final Node root) {
-        Node currentNode = root;
+    private Node<Key, Value> minNode(final Node<Key, Value> root) {
+        Node<Key, Value> currentNode = root;
         while (currentNode.left != null) {
             currentNode = currentNode.left;
         }
@@ -238,7 +262,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
             return Optional.empty();
         }
 
-        final Node max = maxNode(root);
+        final Node<Key, Value> max = maxNode(root);
         return Optional.of(max.key);
     }
 
@@ -248,8 +272,8 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      * @param root Node that represents the root of the tree to search.
      * @return Rightmost (i.e. maximum) node in the tree.
      */
-    private Node maxNode(final Node root) {
-        Node currentNode = root;
+    private Node<Key, Value> maxNode(final Node<Key, Value> root) {
+        Node<Key, Value> currentNode = root;
         while (currentNode.right != null) {
             currentNode = currentNode.right;
         }
@@ -262,14 +286,14 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      */
     @Override
     public Optional<Key> predecessor(final Key key) {
-        final Node startNode = search(key);
+        final Node<Key, Value> startNode = search(key);
 
         // no predecessor if key is not in map
         if (startNode == null) {
             return Optional.empty();
         }
 
-        final Node predecessor = predecessorNode(startNode);
+        final Node<Key, Value> predecessor = predecessorNode(startNode);
 
         if (predecessor != null) {
             return Optional.of(predecessor.key);
@@ -285,7 +309,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      * @param node Node to find the next smallest node of.
      * @return The next smallest node.
      */
-    private Node predecessorNode(final Node node) {
+    private Node<Key, Value> predecessorNode(final Node<Key, Value> node) {
 
         // check if node has left child. predecessor is maximum of tree rooted at left child
         if (node.left != null) {
@@ -293,8 +317,8 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
         }
 
         // otherwise go up the tree
-        Node previousNode = node;
-        Node currentNode = previousNode.parent;
+        Node<Key, Value> previousNode = node;
+        Node<Key, Value> currentNode = previousNode.parent;
 
         // if no parent, there is no predecessor
         // if node was a right child, predecessor node is the parent
@@ -312,14 +336,14 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      */
     @Override
     public Optional<Key> successor(final Key key) {
-        final Node startNode = search(key);
+        final Node<Key, Value> startNode = search(key);
 
         // no successor if key is not in map
         if (startNode == null) {
             return Optional.empty();
         }
 
-        final Node successor = successorNode(startNode);
+        final Node<Key, Value> successor = successorNode(startNode);
 
         if (successor != null) {
             return Optional.of(successor.key);
@@ -335,7 +359,7 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
      * @param node Node to find the next largest node of.
      * @return The next largest node.
      */
-    private Node successorNode(final Node node) {
+    private Node<Key, Value> successorNode(final Node<Key, Value> node) {
 
         // check if node has right child. successor is minimum of tree rooted at right child
         if (node.right != null) {
@@ -343,8 +367,8 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
         }
 
         // otherwise go up the tree
-        Node previousNode = node;
-        Node currentNode = previousNode.parent;
+        Node<Key, Value> previousNode = node;
+        Node<Key, Value> currentNode = previousNode.parent;
 
         // if no parent, there is no successor
         // if node was a left child, successor node is the parent
@@ -354,22 +378,6 @@ public class BSTreeMap<Key extends Comparable<Key>, Value> implements OrderedMap
         }
 
         return currentNode;
-    }
-
-
-    /**
-     * A node in the binary tree, storing a key-value pair and pointers to its parent and children.
-     */
-    private class Node {
-        private Key key;
-        private Value value;
-        private Node parent, left, right;
-
-        public Node(final Key key, final Value value, final Node parent) {
-            this.key = key;
-            this.value = value;
-            this.parent = parent;
-        }
     }
 
 }
